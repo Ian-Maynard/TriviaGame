b697ee3b2d4fb08ec00164a7d0154f05a00bf564
Ian-Maynard <maynard.ian@gmail.com>
2017-03-20 05:10:13 -0400

    HTML wireframe. JavaScript Algol

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+//APP.JS - Trivia Game. 
+// Core loop Algorythithms 
+// After Start  set controlling loops 
+
+$(document).ready(function() {
+
+// Init outer Variables 
+
+	  if (confirm("Do you want to Play Trivia?") == true) {
+                playGame = true;
+            }
+
+// Define Object.
+// Load Question Array 
+
+        
+ while playGame { 
+
+	var rightAns=0;
+	var wrongAns=0;
+	var timedAns=0;
+
+// Load questions objects to array. Assign random order
+// to correct answers in 
+
+		for(var i=0;1<=questions[i];i++){    // Loop through question array
+
+						// Read Question objects 
+						// clear area 
+						// Generate buttons 
+						// Assign attributes
+
+            while (){ 	// Timer controlled loop
+            		 	// Init inner Variables 
+ 						
+ 						// Accept input 
+ 						// Adjudicate answers
+ 						// Output answers 
+ 						// Increment counters 
+ 						// Call Ancilliary functions
+            		} // Time controlled loop 
+            	// Assumed time-up end to loop at this point - Print the 
+            	// print the correct answer.
+
+
+				}// Question controlled Loop 
+
+        }  // Boolean controlled loop 
+
+
+} // Controlling wrapper for Function
