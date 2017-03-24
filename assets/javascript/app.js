47fe67427df4ce409164ed1b9f762dcbc6e7f403
Ian-Maynard <maynard.ian@gmail.com>
2017-03-24 19:51:27 -0400

    Multiple changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,43 +39,86 @@ var qandArray = [
                 playGame = true;
             }
      
+     var qandArray = [
+
+            qandAns {quEst:"Who was the first man in space?",
+                        quAns:["John Glenn", "*Yuri Gagarin", "Gus Grissom","Alexei Leonov"]}
+
+            qandAns {quEst:"Which of these astronauts became a Senator?",
+                        quAns:["*John Glenn*", "John Young", "Gordy Cooper","Neil Armstrong"]}
+
+            qandAns {quEst:"Which of these was the first man to walk in space?",
+                        quAns:["Yuri Gagarin", "John Young", "*Alexey Leonov","Neil Armstrong"]}
+
+            qandAns {quEst:"Which Apollo Mission landed on the moon?",
+                        quAns:["*Apollo 11", "Apollo 5", "Apollo 13","Apollo 9"]}
+
+            qandAns {quEst:"Which of these was the longest serving astronaut?",
+                        quAns:["John Glenn", "*John Young", "Gordy Cooper","Neil Armstrong"]}
+
+            qandAns {quEst:"Which branch of the Military did John Glenn serve in?",
+                        quAns:["*Marines", "Air Force", "Navy","Army Air Corps"]}
+
+            qandAns {quEst:"Who commanded the first shuttle mission?",
+                        quAns:["Eugene Cernan", "Spurgeon Tanner", "Buzz Aldrin","*John Young"]}  
+
+            qandAns {quEst:"Who commanded the last shuttle mission?",
+                        quAns:["Eugene Cernan", "Neil Armstrong", "Buzz Aldrin","*John Young"]}                   
+
+            ];
+
     //Game variable setup        
-    var rightAns = 0;
-    var wrongAns = 0;
-    var timedOutAns = 0;
 
+    var rightAns = 0;               // # of right answers 
+    var wrongAns = 0;               // # of wrong anwers 
+    var timedOutAns = 0;           // Timed Out answers 
+    var gameTime[];               // Game object array
+    var qIndex=0;                 // Current question index 
+    var cAnswer= 0;                //
+    shuffArray(qandArray,gameTime) // Shuffle the storage array into the game-time array
 
-
-     // Load  & Shuffle Question Array  - DONE
 
     while playGame {
 
-    var timeUp = false;     // Time up Boolean Variable 
-    var thisAnswerArray[]; // Shuffled answer array.
-    var  corrIndex;        // Index of the correct answer.
+        var timeUp = false;     // Init Time's-up Boolean Variable 
+        var currAns[];          // Create current Ansswer Array 
+        shuffArray(gametime[qIndex].quAns,currAns); //  Randomize the answer order and shuffle answers into the array.    
+        var correctAns = currAns.indexOf("*") ;         // Index of the correct answer.
 
 
- 
+
             // Clear display area 
 
 
-            // Grab a question and answer off the top   
+            
 
 
-            // Randomize the answer order 
+shuffArray(qandArray,gameDay);
+
+
+            // 
 
 
 
             // Display both 
 
+             var questBtn = $("<button>");
 
+                        if 
+
+                    }
+
+                     questBtn.addClass("quest-button quest-button-color quest");
+                     questBtn.attr("data-quest", printquest);
+                     questBtn.text(printquest);
+                    $("#buttons").append(questBtn);
 
 
             // Start the clock 
 
     
-    var dur = 90; // Duration of the timer
-    var snapCount = setInterval(countDwn,1000); // decfrement and display the timer 
+        var dur = 90; // Duration of the timer
+        var snapCount = setInterval(countDwn,1000); // decrement and display the timer 
 
            
             // Accept input
@@ -85,7 +128,7 @@ var qandArray = [
 
 
 
-            // Disern wether the user wants to quit???
+            // Discern wether the user wants to quit???
             // Load questions objects to array. Assign random order
             // to correct answers in answer array
 
@@ -105,16 +148,7 @@ var qandArray = [
                     // Generate/style question 
                     // Generate buttons 
 
-                     var questBtn = $("<button>");
-
-                        if 
-
-                    }
-
-                     questBtn.addClass("quest-button quest-button-color quest");
-                     questBtn.attr("data-quest", printquest);
-                     questBtn.text(printquest);
-                    $("#buttons").append(questBtn);
+        
 
 
                     // Assign attributes
@@ -140,6 +174,7 @@ var qandArray = [
 
 
 function shuffArray(inPut,outPut) {
+// Shuffle an array randomly to another.
     for (var i = inPut.length - 1; i >= 0; i--) {
         var randomIndex = Math.floor(Math.random() * (i + 1));
         outPut.push(inPut[randomIndex]);
@@ -149,9 +184,9 @@ function shuffArray(inPut,outPut) {
  function countDwn(){
     $("#chron").html(dur);    //Show time in time span
     dur--; //Decrement timer 
-    if (dur <= 10){
-        dur=dur+(dur*232);
-    }
+    if (dur=0){
+        timeUp = true;
+            }
  }
 
 
